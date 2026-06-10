aefa08919c28a83f5140e0b770184935a6944248
RyanHCode <42245712+ryanhcode@users.noreply.github.com>
2026-06-10 11:38:15 -0400

    Mark SubLevelContainerHolder as internal

--- common/src/main/java/dev/ryanhcode/sable/mixinterface/plot/SubLevelContainerHolder.java
+++ common/src/main/java/dev/ryanhcode/sable/mixinterface/plot/SubLevelContainerHolder.java
@@ -1,7 +1,9 @@
 package dev.ryanhcode.sable.mixinterface.plot;
 
 import dev.ryanhcode.sable.api.sublevel.SubLevelContainer;
+import org.jetbrains.annotations.ApiStatus;
 
+@ApiStatus.Internal
 public interface SubLevelContainerHolder {
 
     SubLevelContainer sable$getPlotContainer();
